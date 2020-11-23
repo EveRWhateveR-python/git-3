9ce43daf8ba37a1ccc40efae91935ab8842d918d
EveRWhateveR-python <65110318+EveRWhateveR-python@users.noreply.github.com>
2020-11-23 20:18:09 +0300

    ok? Then...

---
+++ main.py
@@ -0,0 +1,61 @@
+import sys
+from random import randint
+from PyQt5 import QtCore, QtGui, QtWidgets
+from PyQt5.QtWidgets import QApplication, QMainWindow
+from PyQt5.QtGui import QPainter, QColor
+
+
+class UI_Project(QMainWindow):
+    def __init__(self):
+        super().__init__()
+        self.setupUi(self)
+        self.flag = False
+
+    def setupUi(self, MainWindow):
+        MainWindow.setObjectName("MainWindow")
+        MainWindow.resize(525, 335)
+        self.centralwidget = QtWidgets.QWidget(MainWindow)
+        self.centralwidget.setObjectName("centralwidget")
+        self.pushButton = QtWidgets.QPushButton(self.centralwidget)
+        self.pushButton.setGeometry(QtCore.QRect(10, 10, 91, 23))
+        self.pushButton.setObjectName("pushButton")
+        MainWindow.setCentralWidget(self.centralwidget)
+        self.menubar = QtWidgets.QMenuBar(MainWindow)
+        self.menubar.setGeometry(QtCore.QRect(0, 0, 525, 21))
+        self.menubar.setObjectName("menubar")
+        MainWindow.setMenuBar(self.menubar)
+        self.statusbar = QtWidgets.QStatusBar(MainWindow)
+        self.statusbar.setObjectName("statusbar")
+        MainWindow.setStatusBar(self.statusbar)
+
+        self.retranslateUi(MainWindow)
+        QtCore.QMetaObject.connectSlotsByName(MainWindow)
+
+    def retranslateUi(self, MainWindow):
+        _translate = QtCore.QCoreApplication.translate
+        MainWindow.setWindowTitle(_translate("MainWindow", "MainWindow"))
+        self.pushButton.setText(_translate("MainWindow", "Создать круг"))
+        self.pushButton.clicked.connect(self.draw_block)
+
+    def draw_block(self):
+        self.flag = True
+        self.update()
+
+    def paintEvent(self, event):
+        if self.flag:
+            self.qp = QPainter()
+            self.qp.begin(self)
+            self.circle(self.qp)
+            self.qp.end()
+
+    def circle(self, qp):
+        a = randint(1, 100)
+        qp.setBrush(QColor(255, 0, 0))
+        self.qp.drawEllipse(100, 100, 100 + a, 100 + a)
+
+
+if __name__ == '__main__':
+    app = QApplication(sys.argv)
+    ex = UI_Project()
+    ex.show()
+    sys.exit(app.exec())
